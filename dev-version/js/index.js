459cbd687ada6c75782760fd8450c5b830ebbc5e
mohammedeladawi <mohammedeladwe3@gmail.com>
2024-04-24 22:02:24 +0400

    Add projects table

--- dev-version/js/index.js
+++ dev-version/js/index.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
   // Latest Tasks
   const deleteBtns = document.querySelectorAll(".widget--tasks  .delete");
   deleteBtns.forEach((btn) => {
-    console.log(btn.parentElement);
     btn.onclick = () => btn.parentElement.classList.add("deleted");
   });
 });
